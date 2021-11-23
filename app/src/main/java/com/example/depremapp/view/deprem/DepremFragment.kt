c9afc65cb9017aa0ace1eb28d09ff4c42d279370
Mercan Birer <mercan@redline.com.tr>
2021-11-23 17:12:02 +0300

    loading and refresh

--- app/src/main/java/com/example/depremapp/view/deprem/DepremFragment.kt
+++ app/src/main/java/com/example/depremapp/view/deprem/DepremFragment.kt
@@ -1,20 +1,24 @@
 package com.example.depremapp.view.deprem
 
 import android.annotation.SuppressLint
+import android.app.ProgressDialog
+import android.os.Binder
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import androidx.navigation.navGraphViewModels
 import com.example.depremapp.R
 import com.example.depremapp.base.BaseFragment
 import com.example.depremapp.databinding.FragmentDepremBinding
-import com.example.depremapp.model.Deprem
+import com.example.depremapp.extension.hide
+import com.example.depremapp.extension.show
 import com.example.depremapp.view.deprem.adapter.DepremItemAdapter
 import com.github.ajalt.timberkt.e
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_deprem.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import java.util.*
+
 
 @ExperimentalCoroutinesApi
 @FlowPreview
@@ -32,18 +36,24 @@ class DepremFragment : BaseFragment<FragmentDepremBinding>(R.layout.fragment_dep
         super.onViewCreated(view, savedInstanceState)
 
         depremVM.apiDeprem.observe(viewLifecycleOwner, {
-            if (!::depremItemAdapter.isInitialized && it != null) {
-                e { "dede" }
+            if (!::depremItemAdapter.isInitialized && it.isNotEmpty()) {
+                binding.circ.hide()
                 depremItemAdapter = DepremItemAdapter(requireContext(), it)
                 binding.listviewDeprem.adapter = depremItemAdapter
                 depremItemAdapter.notifyDataSetChanged()
                 binding.listviewDeprem.requestFocus()
+                binding.refresh.setOnRefreshListener {
+                    binding.refresh.isRefreshing = false
+                    depremItemAdapter.notifyDataSetChanged()
+                }
             } else if (binding.listviewDeprem.adapter == null) {
                 e { "yok" }
+                binding.circ.show()
                 depremItemAdapter = DepremItemAdapter(requireContext(), it)
                 binding.listviewDeprem.adapter = depremItemAdapter
                 depremItemAdapter.notifyDataSetChanged()
-            } else if (it != null) {
+            } else if (it.isNotEmpty()) {
+                binding.circ.hide()
                 depremItemAdapter.notifyDataSetChanged()
             }
         })
@@ -51,5 +61,6 @@ class DepremFragment : BaseFragment<FragmentDepremBinding>(R.layout.fragment_dep
         e { "deveefevev" }
 
     }
+
 
 }
